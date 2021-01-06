327bf4bf30b50f14fca0e60d72ef72b2a1526ac4
Shehab Gamal <shehab.gamal333@gmail.com>
2021-01-06 23:16:05 +0200

    Adjusting the nav bar background

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const BasicHeader = styled.header`
   justify-content: center;
   align-items: center;
   margin-bottom: 50px;
-  background: transparent;
+  background: ${(props) => props.theme.mainColor};
 `;
 
 function App() {
