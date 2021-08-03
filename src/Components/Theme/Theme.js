453426895e11a4bf9b427319e00c03586b70acdf
Shehab Gamal <shehab.gamal333@gmail.com>
2021-08-03 16:03:29 +0200

    Website isn't live on netlify 1st trial

--- src/Components/Theme/Theme.js
+++ src/Components/Theme/Theme.js
@@ -1,18 +1,3 @@
-export const basic = {
-  borderonhover: "#000",
-  navbackground: "#C5C6C7",
-  fontcolor: "#000",
-  subtleshadow: "#fff",
-  pagebackground: "#fff",
-};
-export const premium = {
-  borderonhover: "#6494",
-  navbackground: "#3554",
-  fontcolor: "#51923",
-  subtleshadow: "#fffbfe",
-  pagebackground: "#634133",
-};
-
 export const calm = {
   primary: "#f8f8f6",
   secondary: "#001730",
